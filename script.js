954a2af7e5142b442e2bf95b2d2ff3d87cdc3bc5
Ryan Safarzadeh <safarzadehryan@gmail.com>
2022-10-15 19:25:49 -0700

    Added search bar function

--- script.js
+++ script.js
@@ -12,9 +12,34 @@ let weather = {
     },
     displayWeather: function(data) {
         const { name } = data;
+        const { country } = data.sys;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name + ", " + country;
+        document.querySelector(".icon").src =
+            "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = Math.round(temp) + "°F";
+        document.querySelector(".wind").innerText = "Wind speed: " + Math.round(speed) + " mph";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-}
+};
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function () {
+        weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Tokyo");
