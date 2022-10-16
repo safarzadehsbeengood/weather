c6b1f8a4963d029069938e95a63f71cf4eba7d8d
Ryan Safarzadeh <safarzadehryan@gmail.com>
2022-10-15 18:28:06 -0700

    Made fetch weather function

---
+++ script.js
@@ -0,0 +1,20 @@
+let weather = {
+    apiKey: "055124159c0459d8db2aa9c00112f2e2",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+    }
+}
